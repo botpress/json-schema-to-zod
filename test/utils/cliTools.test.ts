e4a655462a61aabf973c43da9524a3da9895edf2
allardy <yann.allard1@gmail.com>
2024-03-06 11:02:05 -0500

    remove cli

--- test/utils/cliTools.test.ts
+++
@@ -1,9 +0,0 @@
-import { parseArgs } from "../../src/utils/cliTools";
-import { suite } from "../suite";
-
-suite("cliTools", (test) => {
-  test("parseArgs should handle param as optional wether false or undefined is passed", (assert) => {
-    assert(parseArgs({ test: { required: false } }, []));
-    assert(parseArgs({ test: { required: undefined } }, []));
-  });
-});
